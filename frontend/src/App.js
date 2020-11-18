e4b2480b471631111c7ce0eccb5e612d63eaa615
Amin Aghaee <amin_wdman@yahoo.com>
2020-11-18 17:45:30 -0500

    added react-bootsrap, header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import { Fragment } from "react";
+import { Container } from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+        <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 }
