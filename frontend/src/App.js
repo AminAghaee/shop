1c145dec2554b8dcd3aa0b6ce74752fadf2b9a68
Amin Aghaee <amin_wdman@yahoo.com>
2020-11-22 19:33:32 -0500

    install react-router-dom and react-router-bootsrap, change all a tags to Link tags, create product screen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-import { Fragment } from "react";
-import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-        <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
